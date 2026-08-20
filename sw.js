10d8635a4e55a2aa3a0422ecca1577630ec39c16
zvukor1980-crypto <zvukor1980@gmail.com>
2026-08-20 17:57:12 +0300

    Cache integrated iPhone Center in PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='repairlab-v1';
-const ASSETS=['./','index.html','styles.css','app.js','data/devices.json','manifest.webmanifest'];
+const CACHE='repairlab-v2';
+const ASSETS=['./','index.html','iphone.html','styles.css','app.js','data/devices.json','manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
